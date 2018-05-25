d58a9fa3c3d68fb12d875dc8a8cf3673aee82329
kevbite <kev_bite@msn.com>
2018-05-25 19:03:01 +0100

    Noddy console app.

---
+++ src/Kevsoft.Battleship.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Console 
+
+namespace Kevsoft.Battleship.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Kevsoft.Battleship.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Kevsoft.Battleship.Game;
+
+namespace Kevsoft.Battleship.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Battleships!");
+            Console.WriteLine("=======================");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to get started");
+            Console.ReadKey();
+            var battlefield = new Battlefield(10);
+            var shipPlacer = new ShipPlacer(battlefield);
+            shipPlacer.AddShip(new Game.Battleship(new SingleLineBattleshipPlacement(2)), new ShipPlacement('A', 1, Direction.Across));
+            shipPlacer.AddShip(new Game.Battleship(new SingleLineBattleshipPlacement(1)), new ShipPlacement('B', 2, Direction.Down));
+
+            var battleshipGame = new BattleshipGame(battlefield);
+            
+            while (!battleshipGame.IsComplete)
+            {
+                Console.Clear();
+                Console.Write("Fire at (eg C2): ");
+                var x = Console.ReadKey().KeyChar;
+                var y = (int)char.GetNumericValue(Console.ReadKey().KeyChar);
+
+                battleshipGame.Fire((x, y));
+
+            }
+        }
+    }
+}
